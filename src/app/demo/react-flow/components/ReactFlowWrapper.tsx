49a3a2b55a9ddb4551bd205f86d9f904ed0feb99
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-05-10 17:45:53 +0100

    Removed unused draft versions.

--- src/app/demo/react-flow/components/ReactFlowWrapper.tsx
+++ src/app/demo/react-flow/components/ReactFlowWrapper.tsx
@@ -16,35 +16,9 @@ export const initialEdges = convertDataLinkListToEdgeList(graphDto.closureDtos)
 
 
 export default function ReactFlowWrapper() {
-    // const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    // const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    //
-    // const onConnect = useCallback(
-    //     (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
-    //     [setEdges],
-    // );
 
     return (
         <div style={{width: '100vw', height: '100vh'}}>
-            {/*<ReactFlow*/}
-            {/*    nodes={nodes}*/}
-            {/*    edges={edges}*/}
-            {/*    onNodesChange={onNodesChange}*/}
-            {/*    onEdgesChange={onEdgesChange}*/}
-            {/*    onConnect={onConnect}*/}
-            {/*>*/}
-            {/*    <Controls />*/}
-            {/*    <MiniMap />*/}
-            {/*    <Background variant={BackgroundVariant.Dots} gap={12} size={1} />*/}
-            {/*</ReactFlow>*/}
-            {/*<ReactFlowProvider>*/}
-            {/*<LayoutFlow*/}
-            {/*>*/}
-            {/*    <Controls />*/}
-            {/*    <MiniMap />*/}
-            {/*    <Background variant={BackgroundVariant.Dots} gap={12} size={1} />*/}
-            {/*</LayoutFlow>*/}
-            {/*</ReactFlowProvider>*/}
             <ReactFlowProvider>
                 <LayoutFlowWithForces
                 >

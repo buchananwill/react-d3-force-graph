48ea8a4b55efd0ca836fe2a62b9125c679023c35
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-05-20 11:33:16 +0100

    This build is now working in the demo project.

--- src/components/controllers/ComponentUndefined.tsx
+++ src/components/controllers/ComponentUndefined.tsx
@@ -1,6 +1,9 @@
 import React, { ReactNode } from "react";
 
- 
 export function ComponentUndefined(props: { onClose: () => void }): ReactNode {
-  return <p className={"p-2"}>Content undefined...</p>;
+  return (
+    <p className={"p-2"} onClick={props.onClose}>
+      Content undefined...
+    </p>
+  );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,42 @@
-export * from "./editing";
+export {
+  useEffectSyncToMemo,
+  useEditNodeData,
+  useDeleteNodes,
+  useDeleteLinks,
+  useAddLinks,
+  AddNodesParams,
+  useAddNodes,
+  deDuplicateNames,
+  incrementCloneSuffix,
+  Relation,
+} from "./editing";
 export * from "./literals";
-export * from "./hooks";
+export {
+  useGraphDispatch,
+  useGraphController,
+  useGraphListener,
+  useGraphDispatchAndListener,
+  useGraphEditHooks,
+  useNodeEditing,
+  useD3ForceSimulationMemo,
+  useGraphSelectiveContextKey,
+  defaultDimensionArray,
+  ForceNormalizationCategory,
+  useAllEdits,
+  useDirectSimRefEditsDispatch,
+  useForceAttributeListeners,
+  useGraphEditController,
+  useGraphName,
+  useGraphRefs,
+  useLinkContext,
+  useModalContent,
+  useNodeCloneFunctionController,
+  useNodeContext,
+  useNormalizeForceRange,
+  useShowNodeEditing,
+  useSineLutContext,
+  useSyncRefVersionToNodeAndLinkContext,
+} from "./hooks";
 export * from "./functions/utils";
-export * from "./components";
+export { ForceGraphPage, ComponentUndefined } from "./components";
 export * from "./types";
